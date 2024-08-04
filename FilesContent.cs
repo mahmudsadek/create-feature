1b5e4928a19e6d045355bdc49309e6beac2ecf50
mahmud-sadek <mahmoud.elsadeq@roboost.app>
2024-08-03 22:33:59 -0700

    Add project files.

---
+++ FilesContent.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace create_feature
+{
+    internal class FilesContent
+    {
+        public static string Request(string FeatureName)
+        {
+            StringBuilder request = new StringBuilder();
+            request.Append($"using namespace {MakePlural(FeatureName)}");
+            return request.ToString();
+        }
+
+        static string MakePlural(string word)
+        {
+            if (string.IsNullOrEmpty(word))
+                return word;
+
+            if (word.EndsWith("y") && word.Length > 1 && IsConsonant(word[word.Length - 2]))
+            {
+                return word.Substring(0, word.Length - 1) + "ies";
+            }
+            else if (word.EndsWith("s") || word.EndsWith("x") || word.EndsWith("z") ||
+                     word.EndsWith("ch") || word.EndsWith("sh"))
+            {
+                return word + "es";
+            }
+            else
+            {
+                return word + "s";
+            }
+        }
+        static bool IsConsonant(char c)
+        {
+            return !"aeiou".Contains(char.ToLower(c));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System.IO;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace create_feature
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            var app = new CommandLineApplication
+            {
+                Name = "foldercreator",
+                Description = "Creates a folder or file based on options."
+            };
+
+            app.HelpOption("-?|-h|--help");
+
+            var CommandNameOption = app.Option("-c|--command <FOLDERNAME>", "command name to create.", CommandOptionType.SingleValue);
+            var QueryNameOption = app.Option("-q|--query <FILENAME>", "query name to create.", CommandOptionType.SingleValue);
+
+            app.OnExecute(() =>
+            {
+                string currentDirectory = Directory.GetCurrentDirectory();
+
+                if (CommandNameOption.HasValue())
+                {
+                    CreateFolder(CommandNameOption.Value(), currentDirectory);
+                    CreateFolder("Commands", $"{currentDirectory}/{CommandNameOption.Value()}/");
+                    CreateFile(CommandNameOption.Value() + "Request", $"{currentDirectory}/{CommandNameOption.Value()}/");
+                    CreateFile(CommandNameOption.Value() + "Orchestrator", $"{currentDirectory}/{CommandNameOption.Value()}/");
+                    CreateFile(CommandNameOption.Value() + "Command", $"{currentDirectory}/{CommandNameOption.Value()}/");
+                    CreateFile(CommandNameOption.Value() + "RequestViewModel", currentDirectory);
+                    CreateFile(CommandNameOption.Value() + "ResponseViewModel", currentDirectory);
+                    CreateFile(CommandNameOption.Value() + "Endpoint", currentDirectory);
+
+                }
+
+                if (QueryNameOption.HasValue())
+                {
+                    CreateFolder(QueryNameOption.Value(), currentDirectory);
+                    CreateFolder("Commands", $"{currentDirectory}/{QueryNameOption.Value()}/");
+                    CreateFile(CommandNameOption.Value() + "Request", $"{currentDirectory}/{CommandNameOption.Value()}/");
+                    CreateFile(CommandNameOption.Value() + "Orchestrator", $"{currentDirectory}/{CommandNameOption.Value()}/");
+                    CreateFolder("Queries", $"{currentDirectory}/{QueryNameOption.Value()}/");
+                    CreateFile(CommandNameOption.Value() + "Query", $"{currentDirectory}/{CommandNameOption.Value()}/");
+                    CreateFile(QueryNameOption.Value() + "ResponseViewModel", currentDirectory);
+                    CreateFile(QueryNameOption.Value() + "RequestViewModel", currentDirectory);
+                    CreateFile(QueryNameOption.Value() + "Endpoint", currentDirectory);
+
+                }
+            });
+
+            return app.Execute(args);
+        }
+
+        static void CreateFolder(string folderName, string currentDirectory)
+        {
+            string folderPath = Path.Combine(currentDirectory, folderName);
+            Directory.CreateDirectory(folderPath);
+        }
+
+        static async void CreateFile(string fileName, string currentDirectory, string content = "")
+        {
+            string FilePath = Path.Combine(currentDirectory, fileName);
+            await File.WriteAllTextAsync(FilePath , content);
+        }
+    }
+}
