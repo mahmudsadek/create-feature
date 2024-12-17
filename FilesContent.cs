0d7e734072e9dab19faaf3c27f6f9dd15ebc33ad
mahmudsadek <mahmoudsadek.1905@gmail.com>
2024-12-17 22:47:56 +0200

    fix some err in namesapces

--- FilesContent.cs
+++ FilesContent.cs
@@ -11,8 +11,11 @@ namespace create_feature
         public static string Request(string ModelName, string FeatureName)
         {
             StringBuilder request = new StringBuilder();
-            request.AppendLine($"using MediatR;");
+            request.AppendLine($"using Roboost.Common.Views;");
+            request.AppendLine($"using Roboost.Features.Common;");
+            request.AppendLine($"");
             request.AppendLine($"namespace Roboost.Feature.{MakePlural(ModelName)}.Commands;");
+            request.AppendLine($"");
             request.AppendLine($"public record {FeatureName}Request({FeatureName}RequestViewModel viewModel):IRequest<RequestResult<{FeatureName}ResponseViewModel>>;");
             request.AppendLine($"public class {FeatureName}RequestHandler : RequestHandlerBase<{FeatureName}Request, RequestResult<{FeatureName}ResponseViewModel>>");
             request.AppendLine("{");
@@ -22,9 +25,12 @@ namespace create_feature
             request.AppendLine("    {");
             request.AppendLine($"        var validationResult = ValidateRequest();");
             request.AppendLine("        if (!validationResult.IsSuccess) { return validationResult; }");
-            request.AppendLine($"        var result = await _mediator.Send(new  {FeatureName}Orchestrator());");
+            request.AppendLine($"");
+            request.AppendLine($"        var result = await _mediator.Send(new  {FeatureName}Orchestrator(), cancellationToken).ConfigureAwait(false);");
+            request.AppendLine($"");
             request.AppendLine($"        return result;");
             request.AppendLine("    }");
+            request.AppendLine($"");
             request.AppendLine($"    private RequestResult<{FeatureName}ResponseViewModel> ValidateRequest()");
             request.AppendLine("    {");
             request.AppendLine($"        return RequestResult<{FeatureName}ResponseViewModel>.Success(default, \"\");");
@@ -38,25 +44,28 @@ namespace create_feature
         public static string EndPoint(string ModelName, string FeatureName)
         {
             StringBuilder request = new StringBuilder();
-            request.AppendLine($"using MediatR;");
-            request.AppendLine($"namespace Roboost.Feature.{MakePlural(ModelName)};");
+            request.AppendLine($"using Microsoft.AspNetCore.Mvc;");
+            request.AppendLine($"using Roboost.Common.Views;");
+            request.AppendLine($"using Roboost.Features.Common;");
+            request.AppendLine($"using Roboost.Features.{MakePlural(ModelName)}.Commands;");
+            request.AppendLine($"");
+            request.AppendLine($"namespace Roboost.Features.{MakePlural(ModelName)};");
+            request.AppendLine($"");
             request.AppendLine($"public class {FeatureName}Endpoint : EndpointBase<{FeatureName}RequestViewModel, {FeatureName}ResponseViewModel>");
             request.AppendLine("{");
             request.AppendLine($"    public {FeatureName}Endpoint(EndpointBaseParameters<{FeatureName}RequestViewModel> dependencyCollection) : base(dependencyCollection)");
             request.AppendLine("    {}");
-            request.AppendLine($"    public async Task<EndPointResponse<{FeatureName}ResponseViewModel>> Handle({FeatureName}RequestViewModel request)");
+            request.AppendLine($"    public async Task<EndPointResponse<{FeatureName}ResponseViewModel>> Handle({FeatureName}RequestViewModel request, CancellationToken cancellationToken)");
             request.AppendLine("    {");
-            request.AppendLine($"        var validationResult = await ValidateRequestAsync(request);");
+            request.AppendLine($"        var validationResult = await ValidateRequestAsync(request).ConfigureAwait(false)");
             request.AppendLine("        if (!validationResult.IsSuccess) { return validationResult; }");
-            request.AppendLine($"        var result = await _mediator.Send(new  {FeatureName}Request(request));");
-            request.AppendLine($"        if (result.IsSuccess)");
-            request.AppendLine($"            return EndPointResponse<{FeatureName}ResponseViewModel>.Success(result.Data , \"Message\");");
-            request.AppendLine($"        return EndPointResponse<CreateCityResponseViewModel>.Failure( result.ErrorCode );");
+            request.AppendLine($"");
+            request.AppendLine($"        var result = await _mediator.Send(new  {FeatureName}Request(request), cancellationToken).ConfigureAwait(false);");
+            request.AppendLine($"");
+            request.AppendLine($"        return result.IsSuccess");
+            request.AppendLine($"            ? EndPointResponse<{FeatureName}ResponseViewModel>.Success(result.Data , result.Message);");
+            request.AppendLine($"            : EndPointResponse<{FeatureName}ResponseViewModel>.Failure( result.ErrorCode );");
 
-            request.AppendLine("    }");
-            request.AppendLine($"    private RequestResult<{FeatureName}ResponseViewModel> ValidateRequest()");
-            request.AppendLine("    {");
-            request.AppendLine($"        return RequestResult<{FeatureName}ResponseViewModel>.Success(default, \"\");");
             request.AppendLine("    }");
             request.AppendLine("}");
 
@@ -67,16 +76,18 @@ namespace create_feature
         public static string Orchestrator(string ModelName, string FeatureName, bool isQuery)
         {
             StringBuilder request = new StringBuilder();
-            request.AppendLine($"using MediatR;");
-            request.AppendLine($"namespace Roboost.Feature.{MakePlural(ModelName)}.Commands;");
+            request.AppendLine($"using Roboost.Common.Views;");
+            request.AppendLine($"using Roboost.Features.Common;");
+            request.AppendLine($"");
+            request.AppendLine($"namespace Roboost.Features.{MakePlural(ModelName)}.Commands;");
             request.AppendLine($"public record {FeatureName}Orchestrator():IRequest<RequestResult<{FeatureName}ResponseViewModel>>;");
-            request.AppendLine($"public class {FeatureName}OrchestratorHandler : RequestHandlerBase<{FeatureName}Request, RequestResult<{FeatureName}ResponseViewModel>>");
+            request.AppendLine($"public class {FeatureName}OrchestratorHandler : RequestHandlerBase<{FeatureName}Orchestrator, RequestResult<{FeatureName}ResponseViewModel>>");
             request.AppendLine("{");
             request.AppendLine($"    public {FeatureName}OrchestratorHandler(RequestHandlerBaseParameters parameters) : base(parameters) ");
             request.AppendLine("    {}");
-            request.AppendLine($"    public override async Task<RequestResult<{FeatureName}ResponseViewModel>> Handle({FeatureName}Request request, CancellationToken cancellationToken) ");
+            request.AppendLine($"    public override async Task<RequestResult<{FeatureName}ResponseViewModel>> Handle({FeatureName}Orchestrator request, CancellationToken cancellationToken) ");
             request.AppendLine("    {");
-            request.AppendLine($"        var result = await _mediator.Send(new  {FeatureName}{(isQuery ? "Query()" : "Command()")});");
+            request.AppendLine($"        var result = await _mediator.Send(new  {FeatureName}{(isQuery ? "Query()" : "Command()")}, cancellationToken).ConfigureAwait(false);");
             request.AppendLine($"        return result;");
             request.AppendLine("    }");
             request.AppendLine("}");
@@ -87,16 +98,20 @@ namespace create_feature
         public static string Command(string ModelName, string FeatureName)
         {
             StringBuilder request = new StringBuilder();
-            request.AppendLine($"using MediatR;");
-            request.AppendLine($"namespace Roboost.Feature.{MakePlural(ModelName)}.Commands;");
+            request.AppendLine($"using Roboost.Common.Views;");
+            request.AppendLine($"using Roboost.Features.Common;");
+            request.AppendLine($"using Roboost.Data;");
+            request.AppendLine($"");
+            request.AppendLine($"namespace Roboost.Features.{MakePlural(ModelName)}.Commands;");
+            request.AppendLine($"");
             request.AppendLine($"public record {FeatureName}Command():IRequest<RequestResult<{FeatureName}ResponseViewModel>>;");
-            request.AppendLine($"public class {FeatureName}CommandHandler : RequestHandlerBase<{FeatureName}Request, RequestResult<{FeatureName}ResponseViewModel>>");
+            request.AppendLine($"public class {FeatureName}CommandHandler : RequestHandlerBase<{FeatureName}Command, RequestResult<{FeatureName}ResponseViewModel>>");
             request.AppendLine("{");
             request.AppendLine($"    readonly Repository<YouModelHere> _repository; ");
 
             request.AppendLine($"    public {FeatureName}CommandHandler(RequestHandlerBaseParameters parameters, Repository<YouModelHere> repository) : base(parameters) ");
             request.AppendLine("    { _repository = repository; }");
-            request.AppendLine($"    public override async Task<RequestResult<{FeatureName}ResponseViewModel>> Handle({FeatureName}Request request, CancellationToken cancellationToken) ");
+            request.AppendLine($"    public override async Task<RequestResult<{FeatureName}ResponseViewModel>> Handle({FeatureName}Command request, CancellationToken cancellationToken) ");
             request.AppendLine("    {");
             request.AppendLine("    }");
             request.AppendLine("}");
@@ -108,16 +123,19 @@ namespace create_feature
         public static string Query(string ModelName, string FeatureName)
         {
             StringBuilder request = new StringBuilder();
-            request.AppendLine($"using MediatR;");
-            request.AppendLine($"namespace Roboost.Feature.{MakePlural(ModelName)}.Queries;");
+            request.AppendLine($"using Roboost.Common.Views;");
+            request.AppendLine($"using Roboost.Features.Common;");
+            request.AppendLine($"using Roboost.Data;");
+            request.AppendLine($"");
+            request.AppendLine($"namespace Roboost.Features.{MakePlural(ModelName)}.Queries;");
             request.AppendLine($"public record {FeatureName}Query():IRequest<RequestResult<{FeatureName}ResponseViewModel>>;");
-            request.AppendLine($"public class {FeatureName}QueryHandler : RequestHandlerBase<{FeatureName}Request, RequestResult<{FeatureName}ResponseViewModel>>");
+            request.AppendLine($"public class {FeatureName}QueryHandler : RequestHandlerBase<{FeatureName}Query, RequestResult<{FeatureName}ResponseViewModel>>");
             request.AppendLine("{");
             request.AppendLine($"    readonly Repository<YouModelHere> _repository; ");
 
             request.AppendLine($"    public {FeatureName}QueryHandler(RequestHandlerBaseParameters parameters, Repository<YouModelHere> repository) : base(parameters) ");
             request.AppendLine("    { _repository = repository; }");
-            request.AppendLine($"    public override async Task<RequestResult<{FeatureName}ResponseViewModel>> Handle({FeatureName}Request request, CancellationToken cancellationToken) ");
+            request.AppendLine($"    public override async Task<RequestResult<{FeatureName}ResponseViewModel>> Handle({FeatureName}Query request, CancellationToken cancellationToken) ");
             request.AppendLine("    {");
             request.AppendLine("    }");
             request.AppendLine("}");
@@ -130,7 +148,8 @@ namespace create_feature
         {
             StringBuilder request = new StringBuilder();
 
-            request.AppendLine($"namespace Roboost.Feature.{MakePlural(ModelName)};");
+            request.AppendLine($"using namespace FluentValidation;");
+            request.AppendLine($"namespace Roboost.Features.{MakePlural(ModelName)};");
             request.AppendLine($"public record {FeatureName}RequestViewModel();");
             request.AppendLine($"public class {FeatureName}RequestViewModelValidator : AbstractValidator <{FeatureName}RequestViewModel>");
             request.AppendLine("{");
@@ -147,7 +166,8 @@ namespace create_feature
         {
             StringBuilder request = new StringBuilder();
 
-            request.AppendLine($"namespace Roboost.Feature.{MakePlural(ModelName)};");
+            request.AppendLine($"using Mapster;");
+            request.AppendLine($"namespace Roboost.Features.{MakePlural(ModelName)};");
             request.AppendLine($"public record {FeatureName}ResponseViewModel();");
             request.AppendLine($"public class {FeatureName}ResponseViewModelMappingConfig : IRegister");
             request.AppendLine("{");
